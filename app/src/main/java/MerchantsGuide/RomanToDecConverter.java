0ecaead8c22dfcb50686267b86728756dd27e2bf
ANLU89 <100901600+ANLU89@users.noreply.github.com>
2022-03-07 08:12:14 +0100

    implemented part of the conversion logic

--- app/src/main/java/MerchantsGuide/RomanToDecConverter.java
+++ app/src/main/java/MerchantsGuide/RomanToDecConverter.java
@@ -1,5 +1,6 @@
 package MerchantsGuide;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -69,5 +70,57 @@ public class RomanToDecConverter {
         return validRepeats().contains(c);
     }
 
+    public static int calculateDecValue(String numeral) throws Exception{
 
+        ArrayList<Character> chars = new ArrayList<Character>();
+        
+        for (char c : numeral.toCharArray()){
+            if (isValidCharacter(c)) chars.add(c);
+            else throw new Exception("invalid character!");
+        }
+
+        if (chars.size() == 0) return 0;
+
+        char first = chars.get(0);
+        char second;
+        char max = first;
+        int repeats = 0, sum = 0;
+        int firstVal, secondVal;
+        while (chars.size() >= 1){
+            firstVal = symbols().get(first);
+            if (chars.size() == 1) {
+                sum += firstVal;
+                break;
+            } 
+            second = chars.get(1);
+            secondVal = symbols().get(second);
+            if (secondVal == firstVal){
+                if (!isValidRepeat(first)) throw new Exception(first + " may not be repeated!");
+                else {
+                    sum += firstVal;
+                    repeats++;
+                    if (repeats > 2) throw new Exception("too many repititions of " + first + "!");
+                }
+            }
+            else {
+                repeats = 0;
+                if (firstVal < secondVal) {
+                    if (!isValidSubtraction(first, second)) throw new Exception(first + " may not be subtracted from " + second + "!");
+                    else if (secondVal > symbols().get(max) && symbols().get(max) > firstVal) throw new Exception("invalid order, because " + second + " is larger than " + max + "!");
+                    else {
+                        sum += (secondVal - firstVal);
+                        chars.remove(0);
+                    }
+                }
+                else {
+                    sum += firstVal;
+                    max = second;
+                }
+            }
+            chars.remove(0);
+            if (chars.size() == 0) break;
+            first = chars.get(0);
+        }
+        return sum;
+    }
 }
--- app/src/test/java/MerchantsGuide/RomanToDecConverterTest.java
+++ app/src/test/java/MerchantsGuide/RomanToDecConverterTest.java
@@ -8,4 +8,48 @@ class RomanToDecConverterTest {
     void invalidRepeatofV() {
         assertFalse(RomanToDecConverter.isValidRepeat('V'));
     }
+    @Test 
+    void validRepeatofC() {
+        assertTrue(RomanToDecConverter.isValidRepeat('C'));
+    }
+    @Test 
+    void invalidCharacterlH() {
+        assertFalse(RomanToDecConverter.isValidCharacter('H'));
+    }
+    @Test
+    void validCharacterX() {
+        assertTrue(RomanToDecConverter.isValidCharacter('X'));
+    }
+    @Test
+    void validNumeralIV() throws Exception {
+        assertEquals(4, RomanToDecConverter.calculateDecValue("IV"));
+    }
+    @Test
+    void validNumeralMIX() throws Exception {
+        assertEquals(1009, RomanToDecConverter.calculateDecValue("MIX"));
+    }
+    @Test
+    void validNumeralMMM() throws Exception {
+        assertEquals(3000, RomanToDecConverter.calculateDecValue("MMM"));
+    }
+    @Test
+    void validNumeralCM() throws Exception {
+        assertEquals(900, RomanToDecConverter.calculateDecValue("CM"));
+    }
+    @Test
+    void validNumeralMCM() throws Exception {
+        assertEquals(1900, RomanToDecConverter.calculateDecValue("MCM"));
+    }
+    @Test
+    void validNumeralMCMXLIV() throws Exception {
+        assertEquals(1944, RomanToDecConverter.calculateDecValue("MCMXLIV"));
+    }
+    @Test
+    void validNumeralMMMCMXLIV() throws Exception {
+        assertEquals(3944, RomanToDecConverter.calculateDecValue("MMMCMXLIV"));
+    }
+    @Test
+    void invalidNumeralMCMCM() throws Exception {
+        assertEquals(-1, RomanToDecConverter.calculateDecValue("MCMCM"));
+    }
 }
